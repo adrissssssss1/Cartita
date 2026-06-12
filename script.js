142ce33b38dc777df8bb1940bc357caf8b87be7d
adrissssssss1 <samersyt113@gmail.com>
2026-06-11 23:40:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,33 +4,37 @@ let startY = 0;
 let dragging = false;
 
 function openCover(){
+    cover.style.transition = "transform 0.8s ease";
     cover.style.transform = "translateY(100%)";
 }
 
 cover.addEventListener("pointerdown", (e)=>{
     dragging = true;
     startY = e.clientY;
+
+    cover.style.transition = "none";
 });
 
 cover.addEventListener("pointermove", (e)=>{
-
     if(!dragging) return;
 
     let distance = e.clientY - startY;
-
     if(distance < 0) distance = 0;
 
-    // opcional feedback visual suave
-    cover.style.transform = `translateY(${distance * 0.3}px)`;
+    cover.style.transform = `translateY(${distance * 0.15}px)`;
 
     if(distance > 120){
-        openCover();
         dragging = false;
+        openCover();
     }
 });
 
 cover.addEventListener("pointerup", ()=>{
+    if(!dragging) return;
+
     dragging = false;
+
+    cover.style.transition = "transform 0.5s ease";
     cover.style.transform = "translateY(0)";
 });
 
